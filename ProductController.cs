4f2b5166152f1b1720da2a35ec788d1382027725
NerijusTasks <112897989+NerijusTasks@users.noreply.github.com>
2024-10-24 06:32:53 +0300

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Eshop.Data;
+using Eshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public ProductController (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetProductsAsync")]
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
---
+++ ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eshop.Models
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int CartId { get; set; }
+        public List<CartItem>? Items { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
