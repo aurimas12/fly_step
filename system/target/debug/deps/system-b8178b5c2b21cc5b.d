193d713ecbca6fe33cee7d043a10a19044067407
aurimas <aurimas.butk@gmail.com>
2024-11-04 22:02:57 +0200

    basic rust template

---
+++ system/target/debug/deps/system-b8178b5c2b21cc5b.d
@@ -0,0 +1,5 @@
+/Users/aurimas/Desktop/project/fly_step/system/target/debug/deps/system-b8178b5c2b21cc5b: src/main.rs
+
+/Users/aurimas/Desktop/project/fly_step/system/target/debug/deps/system-b8178b5c2b21cc5b.d: src/main.rs
+
+src/main.rs:
---
+++ system/target/debug/system.d
@@ -0,0 +1,1 @@
+/Users/aurimas/Desktop/project/fly_step/system/target/debug/system: /Users/aurimas/Desktop/project/fly_step/system/src/main.rs
