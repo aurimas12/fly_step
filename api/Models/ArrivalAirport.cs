d3995d381979a218125be6acfa177ade217caa66
Nerijus <Nerijus@DESKTOP-08FUU9C>
2024-11-08 22:11:09 +0200

    Models

---
+++ api/Models/ArrivalAirport.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models
+{
+    public class ArrivalAirport
+    {
+        public string? CountryName { get; set; }
+        public string? IataCode { get; set; }
+        public string? Name { get; set; }
+    }
+}
