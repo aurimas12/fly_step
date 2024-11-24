7ee06946e900317644b058c504e28af06febe408
aurimas <aurimas.butk@gmail.com>
2024-11-24 11:46:11 -0800

    add basic fly ticket UI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use slint::{SharedString, Weak};
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let main_window = MainWindow::new()?;
+    let ui:AppWindow = AppWindow::new()?;
+    let ui_handle:Weak<AppWindow>=ui.as_weak();
+    ui.on_cheapest_fly(move | departure_from:SharedString, departure_to:SharedString, fly_date:SharedString|{
+        println!("From: {}, To: {}, Date: {}", departure_from, departure_to, fly_date);
+        let ui=ui_handle.upgrade().unwrap();
+    /*
+    
+        Ticket api logic
+    
+     */
+    });
 
-    main_window.run()
+    ui.run()
 }
