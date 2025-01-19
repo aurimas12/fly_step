1a55ba655988f48d572daa19daf713df07e55de7
Jurij Jusel <jjusel@gmail.com>
2025-01-19 23:43:52 +0200

    connection pg_admin, created server fly_tickets_db, create tables

--- db/connection.py
+++ db/connection.py
@@ -2,19 +2,25 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
-load_dotenv()
 
-def get_connection():
+load_dotenv(dotenv_path='db/.env')
+
+db_params = {
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
+}
+
+def db_connection(db_params):
     try:
-        connection = psycopg2.connect(
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT")
-        )
-        print("Prisijungimas prie DB sėkmingas.")
+        connection = psycopg2.connect(connect_timeout=10, **db_params)
+        print(f"Prisijungimas prie '{db_params['database']}' DB sėkmingas.")
         return connection
     except psycopg2.Error as e:
         print(f"Prisijungimo klaida: {e}")
+        return None
+    except Exception as e:
+        print(f"An error connecting to the database: {e}")
         return None
