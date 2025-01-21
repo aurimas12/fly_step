883864aedbd3a302cd1dd5a60d047a7d23062ec4
aurimas <aurimas.butk@gmail.com>
2025-01-21 12:51:59 -0800

    add

--- app/src/app/user/user.component.ts
+++ app/src/app/user/user.component.ts
@@ -11,4 +11,14 @@ const randomIndex=Math.floor(Math.random() *DUMMY_USERS.length)
 })
 export class UserComponent {
   selectedUser =DUMMY_USERS[randomIndex]
+
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser(){
+    console.log("click button")
+    const randomIndex=Math.floor(Math.random() *DUMMY_USERS.length)
+    this.selectedUser=DUMMY_USERS[randomIndex];
+  }
 }
